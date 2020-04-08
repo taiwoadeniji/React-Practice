9af7700936e5d39263995ec968ac6788dd1cb479
Taiwo Adeniji <taiwoadeniji19@gmail.com>
2020-04-08 09:30:46 -0600

    created separeted flag.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Button, Icon, Flag, Segment } from "semantic-ui-react";
+import { Button, Icon} from "semantic-ui-react";
 import "./App.css";
-import "./menu.js";
+import MenuExampleSecondary from "./menu.js";
+import FlagExampleFlag from "./flag.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <div className="ui secondary menu">
-          <a className="active item" href="_blank">Home</a>
-          <a class="item" href="_blank">React </a>
-          <a class="item" href="_blank">Javascript</a>
-          <a class="item" href="_blank">Node</a>
-          <a class="item" href="_blank">C#</a>
-          <a class="item" href="_blank">Python</a>
-          
-          
-          <div class="right menu">
-            <div class="item">
-              <div class="ui icon input">
-                <input type="text" placeholder="Search..." />
-                <i aria-hidden="true" class="search icon"></i>
-              </div>
-            </div>
-            <a class="item" href="_blank">Login</a>
-          </div>
-        </div>
-
+        <MenuExampleSecondary />
         <h1>Welcome to React App!</h1>
-        <Segment>
-          <Flag name="ae" />
-          <Flag name="france" />
-          <Flag name="myanmar" />
-        </Segment>
-
+        <FlagExampleFlag />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
         <p>Adding New Text!</p>
         <a
           className="App-link"
@@ -54,11 +30,6 @@ function App() {
           Download
         </Button>
       </header>
-      <div class="ui segment">
-        <i class="ae flag"></i>
-        <i class="france flag"></i>
-        <i class="myanmar flag"></i>
-      </div>
      </div>
   );
 }
--- src/menu.js
+++ src/menu.js
@@ -1,60 +1,58 @@
-import React, { Component } from 'react';
-import { Input, Menu } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Input, Menu } from "semantic-ui-react";
 
 export default class MenuExampleSecondary extends Component {
-  state = { activeItem: 'home' }
+  state = { activeItem: "home" };
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
 
     return (
       <Menu secondary>
         <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
+          name="home"
+          active={activeItem === "home"}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          name='react'
-          active={activeItem === 'react'}
+          name="react"
+          active={activeItem === "react"}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          name='javascript'
-          active={activeItem === 'javascript'}
+          name="javascript"
+          active={activeItem === "javascript"}
           onClick={this.handleItemClick}
         />
-         <Menu.Item
-          name='node'
-          active={activeItem === 'node'}
+        <Menu.Item
+          name="node"
+          active={activeItem === "node"}
           onClick={this.handleItemClick}
         />
-         <Menu.Item
-          name='c#'
-          active={activeItem === 'c#'}
+        <Menu.Item
+          name="c#"
+          active={activeItem === "c#"}
           onClick={this.handleItemClick}
         />
-         <Menu.Item
-          name='python'
-          active={activeItem === 'python'}
+        <Menu.Item
+          name="python"
+          active={activeItem === "python"}
           onClick={this.handleItemClick}
         />
-       
-       
-       
-        <Menu.Menu position='right'>
+
+        <Menu.Menu position="right">
           <Menu.Item>
-            <Input icon='search' placeholder='Search...' />
+            <Input icon="search" placeholder="Search..." />
           </Menu.Item>
           <Menu.Item
-            name='logout'
-            active={activeItem === 'login'}
+            name="logout"
+            active={activeItem === "login"}
             onClick={this.handleItemClick}
           />
         </Menu.Menu>
       </Menu>
-    )
+    );
   }
 }
